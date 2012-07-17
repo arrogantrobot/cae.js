0b7f1717bb301ac4562446f72fa407b4f7d4e069
Rob Long <robert.l.long@gmail.com>
2012-07-16 22:25:03 -0500

    allow 1 cell automaton

--- cae.js
+++ cae.js
@@ -199,7 +199,7 @@ function cae() {
 
     //setter for pixels per cell
     this.setPixelsPerCell = function(pix) {
-        if ((pix > 0) && (pix < Math.min(width,height))) {
+        if ((pix > 0) && (pix <= Math.min(width,height))) {
             pixPerCell = pix;
         }
     }
